817f23dc1931c621dea26e540179c99d6951185a
Preisler25 <preisler.andras@gmial.com>
2022-12-17 21:47:45 +0100

    canvas test

--- frontend/main.js
+++ frontend/main.js
@@ -32,7 +32,10 @@ let changeLobbyDisplay = (mode) => {
 }
 
 let main = () => {
-
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'green';
+    ctx.fillRect(10, 10, 100, 100);
 }
 
 main()
