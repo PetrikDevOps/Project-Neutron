01948afe4b8d6686c6334f385149558f050325bc
Preisler25 <preisler.andras@gmial.com>
2022-12-17 15:11:01 +0100

    lobby fix v3

--- frontend/main.js
+++ frontend/main.js
@@ -10,6 +10,27 @@ let changeRegLog = (wannaReg) => {
     }
 }
 
+let changeLobbyDisplay = (mode) => {
+    let fastGame = document.getElementById('fast-game')
+    let createGame = document.getElementById('lobby-game')
+    let profile = document.getElementById('profile')
+    if (mode === 'fast') {
+        fastGame.style.display = 'flex'
+        createGame.style.display = 'none'
+        profile.style.display = 'none'
+    }
+    else if (mode === 'create') {
+        fastGame.style.display = 'none'
+        createGame.style.display = 'flex'
+        profile.style.display = 'none'
+    }
+    else{
+        fastGame.style.display = 'none'
+        createGame.style.display = 'none'
+        profile.style.display = 'flex'
+    }
+}
+
 let main = () => {
 
 }
